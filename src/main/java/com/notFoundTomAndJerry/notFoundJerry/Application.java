68075f21b63f16c812026abb6dcfa39b4a9d26ca
taeju <tjxown0@gmail.com>
2026-01-19 11:39:43 +0900

    feat: 프로젝트 초기 환경 설정 및 주요 의존성 추가 - JWT(jjwt) 추가: 인증 및 인가 구현을 위한 토큰 라이브러리 - Swagger UI 추가: API 문서 자동화 및 테스트 환경 구축 - Redis & Redisson 추가: 캐싱 및 분산 락 구현을 위한 설정 - Apache Commons Lang3 추가: 랜덤 문자열 생성 등 유틸리티 활용 - Application 수정 - YAML(dev, prod) 프로파일 분리

--- src/main/java/com/notFoundTomAndJerry/notFoundJerry/Application.java
+++ src/main/java/com/notFoundTomAndJerry/notFoundJerry/Application.java
@@ -2,12 +2,19 @@ package com.notFoundTomAndJerry.notFoundJerry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println("\n" +
+				"=================================================\n" +
+				"👮🚨🏃🏽‍➡️ 404 Jerry Application 시작 완료!\n" +
+				"=================================================\n" +
+				"📋 Swagger UI: http://localhost:8080/swagger-ui/index.html\n" +
+				"=================================================\n");
 	}
-
 }

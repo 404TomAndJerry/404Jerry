fef315bc8ef07d71f11d89d9ffe088b402f76a4e
안영진 <dkstl3344@gmail.com>
2026-01-20 16:43:32 +0900

    feat: 공통 에러 응답 DTO 작성 (#2)

---
+++ src/test/java/com/notFoundTomAndJerry/notFoundJerry/global/exception/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.notFoundTomAndJerry.notFoundJerry.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+
+  private final Integer code;   // 서비스 내부 코드 (httpStatus와 분리)
+  private final String message; // 클라이언트 메시지
+}

388fca8eb99b5e1180d8e38ccc97033f0f76e8bd
chaeun99 <hyc9328@naver.com>
2026-01-23 13:19:06 +0900

    feat: LocationRegionResponse 클래스 구현 (#10)

---
+++ src/main/java/com/notFoundTomAndJerry/notFoundJerry/domain/location/dto/response/LocationRegionResponse.java
@@ -0,0 +1,11 @@
+package com.notFoundTomAndJerry.notFoundJerry.domain.location.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LocationRegionResponse {
+
+  private String regionName; // ex) 강남구
+}

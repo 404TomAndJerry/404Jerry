296ea87934b67a5c4b150b5e05c140bd6ec7b7fe
chaeun99 <hyc9328@naver.com>
2026-01-23 11:42:49 +0900

    feat: LocationBoundsRequest 클래스 구현 (#10)

---
+++ src/main/java/com/notFoundTomAndJerry/notFoundJerry/domain/location/dto/request/LocationBoundsRequest.java
@@ -0,0 +1,17 @@
+package com.notFoundTomAndJerry.notFoundJerry.domain.location.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 지도 화면의 사각형 범위(남서단-북동단) 좌표 정보를 담는 DTO
+ */
+@Getter
+@NoArgsConstructor
+public class LocationBoundsRequest {
+
+  private Double minLat; // 남서단 위도
+  private Double minLng; // 남서단 경도
+  private Double maxLat; // 북동단 위도
+  private Double maxLng; // 북동단 경도
+}

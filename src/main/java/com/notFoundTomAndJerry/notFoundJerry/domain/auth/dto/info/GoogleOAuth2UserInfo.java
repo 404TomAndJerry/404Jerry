5c542e21457d5737daebdb3e256cb099be90a0f3
YuJeong <qordbwjd96@gmail.com>
2026-02-02 17:20:49 +0900

    feat: GoogleOAuth2UserInfo 구현 (#29)

---
+++ src/main/java/com/notFoundTomAndJerry/notFoundJerry/domain/auth/dto/info/GoogleOAuth2UserInfo.java
@@ -0,0 +1,26 @@
+package com.notFoundTomAndJerry.notFoundJerry.domain.auth.dto.info;
+
+import java.util.Map;
+
+public record GoogleOAuth2UserInfo(Map<String, Object> attributes) implements OAuth2UserInfo {
+
+  @Override
+  public String getProviderId() {
+    return (String) attributes.get("sub");
+  }
+
+  @Override
+  public String getProvider() {
+    return "google";
+  }
+
+  @Override
+  public String getEmail() {
+    return (String) attributes.get("email");
+  }
+
+  @Override
+  public String getName() {
+    return (String) attributes.get("name");
+  }
+}

b417585bbb9619e1ddcf449688cd5f6380ff29e2
YuJeong <qordbwjd96@gmail.com>
2026-02-02 17:23:56 +0900

    feat: User 엔티티 수정 (#29)

--- src/main/java/com/notFoundTomAndJerry/notFoundJerry/domain/user/entity/User.java
+++ src/main/java/com/notFoundTomAndJerry/notFoundJerry/domain/user/entity/User.java
@@ -1,23 +1,33 @@
 package com.notFoundTomAndJerry.notFoundJerry.domain.user.entity;
 
+import com.notFoundTomAndJerry.notFoundJerry.domain.auth.entity.enums.ProviderType;
 import com.notFoundTomAndJerry.notFoundJerry.domain.user.entity.enums.UserStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-// 인증 주체를 나타내는 사용자
 @Entity
 @Table(name = "users")
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class) // 생성시간 자동 주입
 public class User {
 
   @Id
@@ -28,7 +38,7 @@ public class User {
   private String email;
 
   @Column(length = 255)
-  private String password; // OAuth 사용자는 null
+  private String password;
 
   @Column(nullable = false, unique = true, length = 50)
   private String nickname;
@@ -36,4 +46,15 @@ public class User {
   @Enumerated(EnumType.STRING)
   @Column(nullable = false, length = 20)
   private UserStatus status;
+
+  // ★ 소셜 로그인 종류 (GOOGLE, NAVER 등) 추가
+  @Enumerated(EnumType.STRING)
+  @Column(length = 20)
+  private ProviderType providerType;
+
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
 }

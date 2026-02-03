4bae394475f5ff111c3089bb5712b7e98bb9daa4
taeju <tjxown0@gmail.com>
2026-02-03 14:50:55 +0900

    feat: Error코드 수정

--- src/main/java/com/notFoundTomAndJerry/notFoundJerry/global/exception/CommonErrorCode.java
+++ src/main/java/com/notFoundTomAndJerry/notFoundJerry/global/exception/CommonErrorCode.java
@@ -18,15 +18,7 @@ public enum CommonErrorCode implements ApiCode {
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED.value(), 401, "유효하지 않은 토큰입니다"),
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED.value(), 401, "토큰이 만료되었습니다"),
     FORBIDDEN(HttpStatus.FORBIDDEN.value(), 403, "접근 권한이 없습니다"),
-    ADMIN_REQUIRED(HttpStatus.FORBIDDEN.value(), 403, "관리자 권한이 필요합니다"),
-
-    // CHAT 도메인 추가
-    CHAT_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND.value(), 404, "채팅방을 찾을 수 없습니다."),
-    CHAT_ROOM_NOT_FOUND_ID(404, 404, "채팅방을 찾을 수 없습니다. ID: %d"),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), 404, "해당하는 유저를 찾지 못했습니다."),
-
-    // CHAT 또는 REDIS 관련 도메인에 추가
-    REDIS_PROCESSING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), 500, "메시지 처리 중 오류가 발생했습니다.");
+    ADMIN_REQUIRED(HttpStatus.FORBIDDEN.value(), 403, "관리자 권한이 필요합니다");
 
     private final Integer httpStatus;
     private final Integer code;

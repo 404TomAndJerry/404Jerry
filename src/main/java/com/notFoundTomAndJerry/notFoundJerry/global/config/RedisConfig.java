49934cbee9f7307e8df66b61c15a95a5259e7c4e
서태주 <tjxown0@gmail.com>
2026-02-07 15:52:00 +0900

    Update RedisConfig.java

--- src/main/java/com/notFoundTomAndJerry/notFoundJerry/global/config/RedisConfig.java
+++ src/main/java/com/notFoundTomAndJerry/notFoundJerry/global/config/RedisConfig.java
@@ -31,31 +31,7 @@ public class RedisConfig {
   @Value("${spring.data.redis.password}") // ★ yml에서 비밀번호 읽어오기
   private String password;
 
-  @Bean
-  @Lazy  // 빌드 타임 연결 시도 방지
-  public RedisConnectionFactory redisConnectionFactory() {
-    // 1. 기본 설정 (Host, Port, Password)
-    RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-    config.setHostName(host);
-    config.setPort(port);
-    config.setPassword(password); // ★ 드디어 비밀번호가 주입됩니다!
-
-    // 2. Lettuce 클라이언트 옵션 (Timeout 및 RESP2 강제)
-    LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-        .commandTimeout(Duration.ofSeconds(10))
-        .clientOptions(ClientOptions.builder()
-            .protocolVersion(ProtocolVersion.RESP2) // 인증 실패 방지를 위한 하위 호환성
-            .autoReconnect(true)
-            .pingBeforeActivateConnection(true)  // 연결 전 PING으로 테스트
-            .build())
-        .build();
-
-
-    LettuceConnectionFactory factory = new LettuceConnectionFactory(config, clientConfig);
-    factory.setShareNativeConnection(false);
-    return factory;
-
-  }
+ 
 
   @Bean
   public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
